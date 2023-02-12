7d6a7e547d94b5f82a7abc1a6de23c493cdf24e9
Taylor Fausak <taylor@fausak.me>
2023-02-12 13:05:44 -0500

    Update things for 2023 (#8)
    * Update things for 2023
    
    * Handle pedantic flag
    
    * Fix formatting

--- source/library/Ratel/Wai.hs
+++ source/library/Ratel/Wai.hs
@@ -1,7 +1,8 @@
 module Ratel.Wai
-  ( ratelMiddleware
-  , toRequest
-  ) where
+  ( ratelMiddleware,
+    toRequest,
+  )
+where
 
 import qualified Control.Concurrent as Concurrent
 import qualified Control.Exception as Exception
@@ -14,80 +15,86 @@ import qualified Network.HTTP.Client as Client
 import qualified Network.Wai as Wai
 import qualified Ratel
 
-
-ratelMiddleware
-  :: Ratel.ApiKey
-  -> Maybe Client.Manager
-  -> (Ratel.Payload -> IO Ratel.Payload)
-  -> Wai.Middleware
+ratelMiddleware ::
+  Ratel.ApiKey ->
+  Maybe Client.Manager ->
+  (Ratel.Payload -> IO Ratel.Payload) ->
+  Wai.Middleware
 ratelMiddleware apiKey maybeManager modify handle request respond = do
   Exception.catch
-    (do
-      handle
-        request
-        (\response -> do
-          respond response
-        )
+    ( do
+        handle
+          request
+          ( \response -> do
+              respond response
+          )
     )
-    (\exception -> do
-      let err = Ratel.toError (exception :: Exception.SomeException)
-      let req = toRequest request
-      let
-        server = Ratel.Server
-          { Ratel.serverEnvironmentName = Nothing
-          , Ratel.serverHostname = Nothing
-          , Ratel.serverProjectRoot = Nothing
-          }
-      payload <- modify Ratel.Payload
-        { Ratel.payloadError = err
-        , Ratel.payloadNotifier = Nothing
-        , Ratel.payloadRequest = Just req
-        , Ratel.payloadServer = server
-        }
-      _ <- Concurrent.forkIO
-        (do
-          _ <- Ratel.notify apiKey maybeManager payload
-          return ()
-        )
-      Exception.throwIO exception
+    ( \exception -> do
+        let err = Ratel.toError (exception :: Exception.SomeException)
+        let req = toRequest request
+        let server =
+              Ratel.Server
+                { Ratel.serverEnvironmentName = Nothing,
+                  Ratel.serverHostname = Nothing,
+                  Ratel.serverProjectRoot = Nothing
+                }
+        payload <-
+          modify
+            Ratel.Payload
+              { Ratel.payloadError = err,
+                Ratel.payloadNotifier = Nothing,
+                Ratel.payloadRequest = Just req,
+                Ratel.payloadServer = server
+              }
+        _ <-
+          Concurrent.forkIO
+            ( do
+                _ <- Ratel.notify apiKey maybeManager payload
+                return ()
+            )
+        Exception.throwIO exception
     )
-
 
 toRequest :: Wai.Request -> Ratel.Request
-toRequest request = Ratel.Request
-  { Ratel.requestAction = Nothing
-  , Ratel.requestCgiData = Just
-    (Map.union
-      (Map.fromList
-        [ ("REMOTE_ADDR", show (Wai.remoteHost request))
-        , ("REQUEST_METHOD", BS.unpack (Wai.requestMethod request))
-        ]
-      )
-      (Map.fromList
-        (fmap
-          (\(k, v) ->
-            ( "HTTP_" <> fmap
-              (\c -> if c == '-' then '_' else Char.toUpper c)
-              (BS.unpack (CI.foldedCase k))
-            , BS.unpack v
-            )
+toRequest request =
+  Ratel.Request
+    { Ratel.requestAction = Nothing,
+      Ratel.requestCgiData =
+        Just
+          ( Map.union
+              ( Map.fromList
+                  [ ("REMOTE_ADDR", show (Wai.remoteHost request)),
+                    ("REQUEST_METHOD", BS.unpack (Wai.requestMethod request))
+                  ]
+              )
+              ( Map.fromList
+                  ( fmap
+                      ( \(k, v) ->
+                          ( "HTTP_"
+                              <> fmap
+                                (\c -> if c == '-' then '_' else Char.toUpper c)
+                                (BS.unpack (CI.foldedCase k)),
+                            BS.unpack v
+                          )
+                      )
+                      (Wai.requestHeaders request)
+                  )
+              )
+          ),
+      Ratel.requestComponent = Nothing,
+      Ratel.requestContext = Nothing,
+      Ratel.requestParams =
+        Just
+          ( Map.fromList
+              ( fmap
+                  (Bifunctor.bimap BS.unpack (maybe "" BS.unpack))
+                  (Wai.queryString request)
+              )
+          ),
+      Ratel.requestSession = Nothing,
+      Ratel.requestUrl =
+        Just
+          ( BS.unpack (Wai.rawPathInfo request)
+              <> BS.unpack (Wai.rawQueryString request)
           )
-          (Wai.requestHeaders request)
-        )
-      )
-    )
-  , Ratel.requestComponent = Nothing
-  , Ratel.requestContext = Nothing
-  , Ratel.requestParams = Just
-    (Map.fromList
-      (fmap
-        (Bifunctor.bimap BS.unpack (maybe "" BS.unpack))
-        (Wai.queryString request)
-      )
-    )
-  , Ratel.requestSession = Nothing
-  , Ratel.requestUrl = Just
-    (BS.unpack (Wai.rawPathInfo request)
-    <> BS.unpack (Wai.rawQueryString request)
-    )
-  }
+    }
